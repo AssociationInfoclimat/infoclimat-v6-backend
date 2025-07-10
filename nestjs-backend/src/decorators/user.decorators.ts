eba8af8bda936686b0c8cc1034abc0c1f6969667
Sylvain Touchard <syl20.touchard@gmail.com>
2025-07-11 00:39:44 +0200

    feat: Add an endpoint to get current loggedin user

--- nestjs-backend/src/decorators/user.decorators.ts
+++ nestjs-backend/src/decorators/user.decorators.ts
@@ -2,6 +2,8 @@ import type { ExecutionContext } from '@nestjs/common';
 import { createParamDecorator } from '@nestjs/common';
 import type { ICRequest } from 'src/middlewares/user-auth.middleware';
 
+// See the user-auth.middleware.ts for the middleware that sets the user on the request
+//  and then the auth guard that checks if the user is authenticated (or throws an error)
 export const User = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest() as ICRequest;
